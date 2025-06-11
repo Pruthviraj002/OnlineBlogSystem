2f820ba8ae8cf3797078e6c8b933ea67b538e11a
gitDemo <sy9535393@gmail.com>
2025-06-11 10:49:21 +0530

    create signin api route

--- cilent/src/components/Footer.jsx
+++ cilent/src/components/Footer.jsx
@@ -1,8 +1,6 @@
-// import { Footer } from "flowbite-react";
 
 import {
     Footer,
-    FooterBrand,
     FooterCopyright,
     FooterDivider,
     FooterIcon,
@@ -16,59 +14,6 @@ import { Link } from "react-router-dom";
 
 export default function FooterCom() {
     return (
-        // <Footer color="gray " className="border border-t-8 border-teal-500">
-        //     <div className="">
-        //         <div className="">
-
-        //             <div className="">
-        //                 <div className="w-full">
-        //                     <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-        //                         <div>
-        //                             <FooterBrand
-        //                             />
-
-        //                         </div>
-        //                         <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-        //                             <div>
-        //                                 <FooterTitle title="about" />
-        //                                 <FooterLinkGroup col>
-        //                                     <FooterLink href="#">Flowbite</FooterLink>
-        //                                     <FooterLink href="#">Tailwind CSS</FooterLink>
-        //                                 </FooterLinkGroup>
-        //                             </div>
-        //                             <div>
-        //                                 <FooterTitle title="Follow us" />
-        //                                 <FooterLinkGroup col>
-        //                                     <FooterLink href="#">Github</FooterLink>
-        //                                     <FooterLink href="#">Discord</FooterLink>
-        //                                 </FooterLinkGroup>
-        //                             </div>
-        //                             <div>
-        //                                 <FooterTitle title="Legal" />
-        //                                 <FooterLinkGroup col>
-        //                                     <FooterLink href="#">Privacy Policy</FooterLink>
-        //                                     <FooterLink href="#">Terms &amp; Conditions</FooterLink>
-        //                                 </FooterLinkGroup>
-        //                             </div>
-        //                         </div>
-        //                     </div>
-        //                     <FooterDivider />
-        //                     <div className="w-full sm:flex sm:items-center sm:justify-between">
-        //                         <FooterCopyright href="#" by="Flowbite™" year={2022} />
-        //                         <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-        //                             <FooterIcon href="#" icon={BsFacebook} />
-        //                             <FooterIcon href="#" icon={BsInstagram} />
-        //                             <FooterIcon href="#" icon={BsTwitter} />
-        //                             <FooterIcon href="#" icon={BsGithub} />
-        //                             <FooterIcon href="#" icon={BsDribbble} />
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </Footer >
-
         <Footer color="white" className=" p-3 border border-t-8 border-teal-500">
             <div className="w-full max-w-7xl mx-auto">
                 <div className="grid w-full justify-between sm:flex md:grid-cols-1">
@@ -82,7 +27,6 @@ export default function FooterCom() {
                         <div>
                             <FooterTitle title="about" >
                             </FooterTitle>
-
                             <FooterLinkGroup col >
                                 <FooterLink href="#" target="_blank" role="noopen noreferrer" >
                                     project
@@ -91,12 +35,10 @@ export default function FooterCom() {
                                     blog
                                 </FooterLink>
                             </FooterLinkGroup>
-
                         </div>
                         <div>
                             <FooterTitle title="follow us" >
                             </FooterTitle>
-
                             <FooterLinkGroup col >
                                 <FooterLink href="#" target="_blank" role="noopen noreferrer" >
                                     github
@@ -106,11 +48,9 @@ export default function FooterCom() {
                                 </FooterLink>
                             </FooterLinkGroup>
                         </div>
-
                         <div>
                             <FooterTitle title="legal" >
                             </FooterTitle>
-
                             <FooterLinkGroup col >
                                 <FooterLink href="#">
                                     privary policy
