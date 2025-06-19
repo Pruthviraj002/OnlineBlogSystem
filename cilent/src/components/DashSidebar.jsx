93cdaf25f5c6a4edafee3bb529ccdfb5b0ed87d4
gitDemo <sy9535393@gmail.com>
2025-06-19 23:28:08 +0530

    complete sidebar of the dashboard

---
+++ cilent/src/components/DashSidebar.jsx
@@ -0,0 +1,35 @@
+import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const tabFormUrl = urlParams.get('tab')
+        // console.log(tabFormUrl)
+        if (tabFormUrl) {
+            setTab(tabFormUrl)
+        }
+    }, [location.search])
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <SidebarItems>
+                <SidebarItemGroup>
+                    <Link to='dashboard?tab=profile'>
+                        <SidebarItem active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                            profile
+                        </SidebarItem>
+                    </Link>
+
+                    <SidebarItem icon={HiArrowSmRight} className='cursor-pointer'>
+                        sign out
+                    </SidebarItem>
+                </SidebarItemGroup>
+            </SidebarItems>
+        </Sidebar>
+    )
+}
